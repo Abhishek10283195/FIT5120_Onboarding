1c6e82deb5a566deee9f6532ddd8e2d92493975a
anag0014 <anag0014@student.monash.edu>
2022-03-13 14:16:31 +1100

    User story 1

---
+++ FIT5122/Controllers/RecycleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5122.Controllers
+{
+    public class RecycleController : Controller
+    {
+        // GET: Recycle
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
